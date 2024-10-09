6de69f3e59c938dbe16d0a60050b420eb28978c2
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-10-09 14:17:21 +0500

    EF and AppDbcontext

---
+++ Extensions/AppServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OsonAptekaFastEndpoints.Data;
+
+namespace OsonAptekaFastEndpoints.Extensions
+{
+    public static class AppServiceExtensions
+    {
+        public static IServiceCollection AddAppService(this IServiceCollection services,IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DbConnection"));
+            });
+            return services;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using OsonAptekaFastEndpoints.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAppService(builder.Configuration);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
