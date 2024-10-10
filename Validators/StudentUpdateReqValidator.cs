8f1e481fa555427e6839e25d7b5f16645f2182bb
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-10-10 15:33:31 +0500

    validators

---
+++ Validators/StudentUpdateReqValidator.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using OsonAptekaFastEndpoints.RequestDtos.Students;
+
+namespace OsonAptekaFastEndpoints.Validators
+{
+    public class StudentUpdateReqValidator : Validator<StudentUpdateReq>
+    {
+        public StudentUpdateReqValidator()
+        {
+            RuleFor(x => x.FullName)
+                .NotEmpty()
+                .WithMessage("To'liq ism kiritishingiz shart")
+                .Length(5, 250)
+                .WithMessage("Ism eng kamida 5ta eng kopida 250ta belgidan iborat bolish kerak");
+            RuleFor(x => x.BirthDate)
+                .NotEmpty()
+                .WithMessage("tugilgan kun kiritish shart");
+            RuleFor(x => x.ClassId)
+                .NotEmpty();
+
+
+        }
+    }
+}
